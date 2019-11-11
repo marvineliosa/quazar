9ea6a626ce5fc85d4693994b6af0b647f6fb6921
Axel <axel170995@gmail.com>
2019-11-11 17:43:17 -0600

    agregado de registros de ejemplo en la tabla de socios

--- resources/views/listado_socios.blade.php
+++ resources/views/listado_socios.blade.php
@@ -1,6 +1,6 @@
 @extends('plantillas.menu')
 @section('content')
-	
+
   <div class="col-md-12">
       <div class="card">
           <div class="card-header">
@@ -25,6 +25,26 @@
                           <td>2221924430</td>
                           <td>Q2</td>
                           <td>Q16</td>
+                          <td></td>
+                      </tr>
+                  </tbody>
+									<tbody>
+                      <tr>
+                          <td>Q19</td>
+                          <td>Leonardo Axel Hernandez Rancaño</td>
+                          <td>221200</td>
+                          <td>Q1</td>
+                          <td>Q15</td>
+                          <td></td>
+                      </tr>
+                  </tbody>
+									<tbody>
+                      <tr>
+                          <td>Q20</td>
+                          <td>Gerardo Quechol</td>
+                          <td>2212364589</td>
+                          <td>Q11</td>
+                          <td>Q19</td>
                           <td></td>
                       </tr>
                   </tbody>
